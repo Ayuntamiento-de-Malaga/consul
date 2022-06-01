20ca64209585c93b9196fc9585cdb44d35c4d2b7
Diego Calvo <diegocc6@gmail.com>
2022-06-01 10:53:38 +0200

    debug5

--- lib/remote_census_rest_api.rb
+++ lib/remote_census_rest_api.rb
@@ -82,6 +82,7 @@ class RemoteCensusRestApi
       if end_point_defined?
         request = request(document_type, document_number, date_of_birth, postal_code)
         headers = {'Content-Type' => 'application/json; charset=utf-8'},
+
         response = RestClient::Request.execute(method: :post, headers: headers, url: Setting["remote_census.general.endpoint"],
                             payload: "#{request.to_json}")
         body = JSON.parse(response.body, :symbolize_names => true)

587c0df40448aa579873b269b405d15ecc97158f
Diego Calvo <diegocc6@gmail.com>
2022-06-06 11:28:25 +0200

    logs for debug

--- lib/remote_census_rest_api.rb
+++ lib/remote_census_rest_api.rb
@@ -79,9 +79,9 @@ class RemoteCensusRestApi
       if end_point_defined?
         request = request(document_type, document_number, date_of_birth, postal_code)
         headers = {'Content-Type' => 'application/json; charset=utf-8'}
-        puts 'REQUEST!'
-        puts request
-        puts request.to_json
+        logger = Rails.logger
+        logger.info request
+        logger.info request.to_json
         response = RestClient::Request.execute(method: :post, headers: headers, url: Setting["remote_census.general.endpoint"],
                             payload: "#{request.to_json}")
         body = JSON.parse(response.body, :symbolize_names => true)

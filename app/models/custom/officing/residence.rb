a431824996ab532fd3a0c5469c649827e7fd3356
Diego Calvo <diego.calvo@enreda.coop>
2025-06-26 11:59:59 +0200

    custom save method for Officing::Residence

--- app/models/custom/officing/residence.rb
+++ app/models/custom/officing/residence.rb
@@ -2,6 +2,30 @@ require_dependency Rails.root.join("app", "models", "officing", "residence").to_
 
 class Officing::Residence
 
+  def save
+    return false unless valid?
+
+    if user_exists?
+      self.user = find_user_by_document
+      user.update!(verified_at: Time.current)
+    else
+      user_params = {
+        document_number: document_number,
+        document_type: document_type,
+        geozone: geozone,
+        date_of_birth: response_date_of_birth&.in_time_zone.to_datetime,
+        gender: gender,
+        residence_verified_at: Time.current,
+        verified_at: Time.current,
+        erased_at: Time.current,
+        password: random_password,
+        terms_of_service: "1",
+        email: nil
+      }
+      self.user = User.create!(user_params)
+    end
+  end
+
   def allowed_age
     return if errors[:year_of_birth].any?
     return unless @census_api_response.valid?
